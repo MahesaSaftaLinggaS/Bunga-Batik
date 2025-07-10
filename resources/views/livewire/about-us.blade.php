ede334fdc4613a538ce1c5c3c9f7f98a275253a9
MahesaSaftaLinggaS <mahesa67ls@gmail.com>
2025-07-10 12:07:18 +0700

    tentang kami

--- resources/views/livewire/about-us.blade.php
+++ resources/views/livewire/about-us.blade.php
@@ -6,10 +6,10 @@
     <div class="mt-5 lg:mt-16 grid lg:grid-cols-3 gap-8 lg:gap-12">
       <div class="lg:col-span-1">
         <h2 class="font-bold text-2xl md:text-3xl text-gray-800 dark:text-neutral-200">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius facilis omnis, eos at deleniti voluptas
+          Tentang Kami – Toko Batik Bunga
         </h2>
         <p class="mt-2 md:mt-4 text-gray-500 dark:text-neutral-500">
-          Lorem, ipsum Lorem ipsum dolor sit amet Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus.
+           Kami adalah tim pengembang yang berfokus pada digitalisasi UMKM, khususnya toko pakaian lokal. Aplikasi ini dibangun untuk memudahkan pelanggan berbelanja batik secara online dengan pengalaman yang nyaman dan efisien. Kami percaya bahwa teknologi harus bisa diakses dan dimanfaatkan oleh semua, termasuk pelaku usaha kecil agar dapat bersaing di era digital.
         </p>
       </div>
       <!-- End Col -->
@@ -20,11 +20,11 @@
           <div class="flex gap-x-5">
             <svg class="shrink-0 mt-1 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="10" x="3" y="11" rx="2"/><circle cx="12" cy="5" r="2"/><path d="M12 7v4"/><line x1="8" x2="8" y1="16" y2="16"/><line x1="16" x2="16" y1="16" y2="16"/></svg>
             <div class="grow">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                Creative minds
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-black">
+                Alamat
               </h3>
               <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                We choose our teams carefully. Our people are the secret to great work.
+                pasar baru trade center bandung , Lt.3 B2 no.96
               </p>
             </div>
           </div>
@@ -34,11 +34,17 @@
           <div class="flex gap-x-5">
             <svg class="shrink-0 mt-1 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 10v12"/><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"/></svg>
             <div class="grow">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                Simple and affordable
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-black">
+                Layanan Toko kami 
               </h3>
               <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                From boarding passes to movie tickets, there's pretty much nothing you can't store with Preline.
+                Penjualan Produk Batik dan Kebaya
+              </p>
+              <p class="mt-1 text-gray-600 dark:text-neutral-400">
+                layanan pembeli online & Offline
+              </p>
+              <p class="mt-1 text-gray-600 dark:text-neutral-400">
+                Rekomendasi Ukuran & Gaya Model
               </p>
             </div>
           </div>
@@ -48,12 +54,11 @@
           <div class="flex gap-x-5">
             <svg class="shrink-0 mt-1 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
             <div class="grow">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                Industry-leading documentation
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-black">
+                No Hp
               </h3>
               <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                Our documentation and extensive Client libraries contain everything a business needs to build a custom integration.
-              </p>
+                081220541576            </p>
             </div>
           </div>
           <!-- End Icon Block -->
@@ -62,11 +67,12 @@
           <div class="flex gap-x-5">
             <svg class="shrink-0 mt-1 size-6 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
             <div class="grow">
-              <h3 class="text-lg font-semibold text-gray-800 dark:text-white">
-                Designing for people
+              <h3 class="text-lg font-semibold text-gray-800 dark:text-black">
+                Sosiail Media Kami
               </h3>
               <p class="mt-1 text-gray-600 dark:text-neutral-400">
-                We actively pursue the right balance between functionality and aesthetics, creating delightful experiences.
+              Tiktok @bungaa.batik
+              Instagram @bungaa.batik
               </p>
             </div>
           </div>
