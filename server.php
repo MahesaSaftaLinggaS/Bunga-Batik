9c7d5ced56df4f6fc9bc62bbbc708fca2e89d72d
MahesaSaftaLinggaS <mahesa67ls@gmail.com>
2025-07-11 12:44:53 +0700

    server

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+
+// Router khusus untuk Railway agar Laravel tetap handle request
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
